0f993f863aabb717e03605eef43119a11222ef4b
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-25 13:26:39 +0100

    Stash work

--- 4-pokedex/app/routes/home.tsx
+++ 4-pokedex/app/routes/home.tsx
@@ -1,4 +1,6 @@
+import { Box, Card, Grid2, Typography } from "@mui/material";
 import type { Route } from "./+types/home";
+import { pokedex, type Pokemon } from '../data/pokedex'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,6 +9,29 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+type PokemonItemProps = {
+  pokemon: Pokemon
+}
+
+function PokemonItem(props: PokemonItemProps) {
+  const pokemon = props.pokemon
+
+  return <Card style={{ padding: 12 }}>
+    <img src={pokemon.sprites.front_default || ""} />
+    <Typography variant="h3">{pokemon.name}</Typography>
+
+    <img src={pokemon.types[0].icon} style={{}} />
+  </Card>
+}
+
 export default function Home() {
-  return <p>To jest strona domowa</p>;
+  return <Box>
+    <Grid2 container spacing={4} size="auto">
+      {
+        pokedex.map((pokemon, index) => {
+          return <PokemonItem pokemon={pokemon} key={index}/>
+        })
+      }
+    </Grid2>
+  </Box>
 }
